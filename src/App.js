63b21e0cdbad60509b080bf3004b9c9767068ee5
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-07-09 12:12:05 -0300

    inserindo o css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Pessoa from './components/Pessoa';
+import SayMyName from './components/SayMyName';
+import Frase from './components/Frase';
 
 function App() {
+  const nome = "Maria"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Estilo CSS</h1>
+      <Frase/>
+      <SayMyName nome="Edson" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Edson" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
+
+
     </div>
+
   );
 }
-
 export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,10 @@
+import styles from './Frase.module.css'
+
+function Frase() {
+    return (
+        <div ClassName={styles.fraseContainer}> 
+            <p>Este é um componente com uma frase!</p>
+        </div>
+    )
+}
+export default Frase
